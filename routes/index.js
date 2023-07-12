d990e78fc51d88cfce5cc470fe5bef6205547d4b
Edward Rudd <urkle@outoforder.cc>
2023-07-12 11:03:37 -0400

    feat: Cleanup index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Node.JS Server demo'});
 });
 
 module.exports = router;
